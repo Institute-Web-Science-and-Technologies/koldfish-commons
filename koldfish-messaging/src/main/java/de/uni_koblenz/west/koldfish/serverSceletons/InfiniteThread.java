b97b6f7d6f2c0dcc48b19a865c62f299f3fb5e56
Daniel Janke <danijank@uni-koblenz.de>
2016-03-17 16:44:49 +0100

    added infinite Thread

---
+++ koldfish-messaging/src/main/java/de/uni_koblenz/west/koldfish/serverSceletons/InfiniteThread.java
@@ -0,0 +1,66 @@
+package de.uni_koblenz.west.koldfish.serverSceletons;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public abstract class InfiniteThread extends Thread implements AutoCloseable {
+
+	protected final Logger log = LogManager.getLogger(getClass());
+
+	private volatile boolean isInterrupted;
+
+	public InfiniteThread(boolean addShutdownHook) {
+		isInterrupted = false;
+		if (addShutdownHook) {
+			Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+				@Override
+				public void run() {
+					isInterrupted = false;
+					if (isAlive()) {
+						interrupt();
+					}
+					try {
+						InfiniteThread.this.join();
+					} catch (InterruptedException e) {
+						log.error(e);
+					}
+					try {
+						close();
+					} catch (Exception e) {
+						log.error(e);
+					}
+				}
+			}));
+		}
+	}
+
+	@Override
+	public void run() {
+		while (!isInterrupted()) {
+			performOneIterationStep();
+		}
+	}
+
+	public abstract void performOneIterationStep();
+
+	@Override
+	public boolean isInterrupted() {
+		return super.isInterrupted() || isInterrupted;
+	}
+
+	@Override
+	public void interrupt() {
+		isInterrupted = true;
+		super.interrupt();
+	}
+
+	@Override
+	public void close() throws Exception {
+		if (isAlive()) {
+			interrupt();
+		} else {
+			isInterrupted = true;
+		}
+	}
+
+}
