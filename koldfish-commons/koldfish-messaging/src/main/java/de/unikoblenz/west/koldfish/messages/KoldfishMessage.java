f3cd59afb034dedc3328f9e367ae6e7eca14083f
Leon Kastler <lkastler@uni-koblenz.de>
2016-06-13 15:07:42 +0200

    moved repositories and stuff

--- koldfish-commons/koldfish-messaging/src/main/java/de/unikoblenz/west/koldfish/messages/KoldfishMessage.java
+++ koldfish-commons/koldfish-messaging/src/main/java/de/unikoblenz/west/koldfish/messages/KoldfishMessage.java
@@ -2,6 +2,12 @@ package de.unikoblenz.west.koldfish.messages;
 
 import java.io.Serializable;
 
+/**
+ * base interface for Koldfish JMS messages
+ * 
+ * @author lkastler
+ *
+ */
 public interface KoldfishMessage extends Serializable {
 
 }
