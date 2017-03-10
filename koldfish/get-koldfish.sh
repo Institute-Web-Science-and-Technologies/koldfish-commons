9b131061d08aa0a5abc5623df09b7bdd90a38624
Daniel Janke <danijank@uni-koblenz.de>
2017-03-10 10:39:58 +0100

    adapted URL of repository

--- koldfish/get-koldfish.sh
+++ koldfish/get-koldfish.sh
@@ -5,6 +5,6 @@ dependency_get=org.apache.maven.plugins:maven-dependency-plugin:2.1:get
 koldfish_groupId=de.unikoblenz.west.koldfish
 koldfish_artifactId=koldfish
 koldfish_version=0.0.1
-repo_uri=https://maven.west.uni-koblenz.de:8443/repository/koral/releases
+repo_uri=https://maven.west.uni-koblenz.de:8443/repository/koral
 
 ${mvn} ${mvn_parameters} ${dependency_get} -Dartifact=${koldfish_groupId}:${koldfish_artifactId}:${koldfish_version}:pom  -DrepoUrl=${repo_uri}
